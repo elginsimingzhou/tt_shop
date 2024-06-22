6c33036791b8904685a5665b727828780d99dae9
Si Ming Zhou <elginsi2000@gmail.com>
2024-06-22 13:59:57 +0800

    added and tested some middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const PORT = 3000 || process.env.PORT
+const PORT = 3000 || process.env.PORT;
 
 const app = express();
 
+//Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.json("Hello world");
-})
+app.get("/", (req, res) => {
+    console.log('home')
+  res.json("Hello world");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`App listening at PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening at PORT ${PORT}`);
+});
