23bcccaf825ec0104cae02ddc96d1e893d1aad66
Si Ming Zhou <elginsi2000@gmail.com>
2024-06-24 13:57:28 +0800

    added tailwindcss frontend, awscloudfront (cdn) integration, home route for database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const PORT = 3000 || process.env.PORT;
 
@@ -13,10 +15,26 @@ app.use(
 );
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log('home')
-  res.json("Hello world");
+app.get("/", async (req, res) => {
+  // res.json("Hello world");
+  const videos = await pool.query("SELECT * FROM videos");
+  const videos_url = [];
+  for (let i = 0; i < videos.rows.length; i++) {
+    videos_url.push(videos.rows[i].video_url);
+  }
+  console.log('videos pushed');
+  res.status(200).json(videos_url)
 });
+
+//GET: Retrieve specific video
+app.get("/videos/:video_id", (req, res) => {});
+
+//GET: Retrieve all products to load TikTok Shop
+//Contain recommender system logic
+app.get("/products", (req, res) => {});
+
+//GET: Retrieve specific product resource when loading product page
+app.get("/products/:product_id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`App listening at PORT ${PORT}`);
