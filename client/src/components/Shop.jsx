933f24ef150f375ce3ede1bae4181fd0781b6956
Si Ming Zhou <elginsi2000@gmail.com>
2024-06-26 13:02:16 +0800

    Integrated /products/:product_id endpoint into frontend

--- client/src/components/Shop.jsx
+++ client/src/components/Shop.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 const Shop = () => {
+  const { product_id } = useParams();
+  const [product, setProduct] = useState({});
+  // const location = useLocation() ;
+  // const productState = location.state;
 
-  const location = useLocation() ;
-  const product = location.state;
+  useEffect(() => {
+    async function fetchProduct() {
+      const response = await fetch(
+        `http://localhost:3000/products/${product_id}`
+      );
+      const fetchedProduct = await response.json();
+      setProduct(fetchedProduct);
+    }
+    fetchProduct();
+  }, []);
+
   return (
     <div>
-        This is a specific product: {product.title}
-        <img  src={`${import.meta.env.VITE_AWSCLOUDFRONT_PRODUCT_URL}/products/${product.image_url}.jpg`} />
+      {Object.keys(product).length !== 0 && (
+        <div>
+          <h1>
+            This is a specific product: {product.product_info.product_title}
+          </h1>
+          <img
+            src={`${import.meta.env.VITE_AWSCLOUDFRONT_PRODUCT_URL}/products/${
+              product.product_info.image_url
+            }.jpg`}
+          />
+          <p>Rating Count: {product.metrics.rating_count}</p>
+          <p>Average Rating: {product.metrics.avg_ratings}</p>
+          <p>Review Count: {product.metrics.review_count}</p>
+          <p>Reviews: {JSON.stringify(product.reviews)}</p>
+        </div>
+      ) }
     </div>
-  )
-}
+  );
+};
 
-export default Shop
-    
+export default Shop;
