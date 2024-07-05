867236df96e0edfba6ea068d42c31e9fefaa7dff
Marcus <marcus.yks27@gmail.com>
2024-07-05 23:16:34 +0800

    Individual product

--- client/src/components/Shop.jsx
+++ client/src/components/Shop.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ReplyIcon from '@mui/icons-material/Reply';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 const Shop = () => {
   const { product_id } = useParams();
@@ -8,32 +13,50 @@ const Shop = () => {
   // const productState = location.state;
 
   useEffect(() => {
-    async function fetchProduct() {
-      const response = await fetch(
-        `http://localhost:3000/products/${product_id}`
-      );
-      const fetchedProduct = await response.json();
-      setProduct(fetchedProduct);
-    }
-    fetchProduct();
+    // async function fetchProduct() {
+    //   const response = await fetch(
+    //     `http://localhost:3000/products/${product_id}`
+    //   );
+    //   const fetchedProduct = await response.json();
+    //   setProduct(fetchedProduct);
+    // }
+    // fetchProduct();
   }, []);
 
   return (
     <div>
       {Object.keys(product).length !== 0 && (
         <div>
-          <h1>
-            This is a specific product: {product.product_info.product_title}
-          </h1>
+          <div className="p-4 flex justify-between">
+            <Link to="/products" className="text-black">
+              <ArrowBackIosIcon fontSize="large"/>
+            </Link>
+            <div className="flex gap-3">
+              <ReplyIcon fontSize="large"/>
+              <ShoppingCartIcon fontSize="large"/>
+              <MoreHorizIcon fontSize="large"/>
+            </div>
+          </div>
           <img
             src={`${import.meta.env.VITE_AWSCLOUDFRONT_PRODUCT_URL}/products/${
               product.product_info.image_url
             }.jpg`}
           />
-          <p>Rating Count: {product.metrics.rating_count}</p>
-          <p>Average Rating: {product.metrics.avg_ratings}</p>
-          <p>Review Count: {product.metrics.review_count}</p>
-          <p>Reviews: {JSON.stringify(product.reviews)}</p>
+          <div className="text-left p-2">
+            <p className="font-bold text-lg">S${product.product_info.price}</p>
+            <div className="flex">
+              {product.product_info.product_title}
+              <BookmarkBorderIcon/>
+            </div>
+            <div>
+              {product.product_info.product_sold_count} sold
+            </div>
+
+            <p>Rating Count: {product.metrics.rating_count}</p>
+            <p>Average Rating: {product.metrics.avg_ratings}</p>
+            <p>Review Count: {product.metrics.review_count}</p>
+            <p>Reviews: {JSON.stringify(product.reviews)}</p>
+          </div>
         </div>
       ) }
     </div>
